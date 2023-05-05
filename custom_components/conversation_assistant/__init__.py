e6711fbdf6a21fccf728b0024dd4a6edb99d3628
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2023-05-05 23:56:18 +0800

    支持HA 5月份的版本

--- custom_components/conversation_assistant/__init__.py
+++ custom_components/conversation_assistant/__init__.py
@@ -11,15 +11,16 @@ from homeassistant.core import HomeAssistant, Context
 from homeassistant.exceptions import ConfigEntryNotReady, TemplateError
 from homeassistant.helpers import intent, template
 from homeassistant.util import ulid
+from home_assistant_intents import get_domains_and_languages, get_intents
 
-from .conversation import Conversation
+from .conversation_assistant import ConversationAssistant
 DATA_VOICE = "conversation_voice"
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
-    assistant = ConversationAssistant(hass, entry)
+    assistant = ConversationAssistantAgent(hass, entry)
     conversation.async_set_agent(hass, entry, assistant)
 
     async def recognize(text, conversation_id=None):
@@ -31,7 +32,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             )
         )
         return result
-    hass.data[DATA_VOICE] = Conversation(hass, recognize)
+    hass.data[DATA_VOICE] = ConversationAssistant(hass, recognize)
     return True
 
 
@@ -40,13 +41,12 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-class ConversationAssistant(conversation.AbstractConversationAgent):
+class ConversationAssistantAgent(conversation.AbstractConversationAgent):
 
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
         """Initialize the agent."""
         self.hass = hass
         self.entry = entry
-        self.history: dict[str, list[dict]] = {}
         # 预定义实体
         self.calendar_id = self.entry.options.get("calendar_id")
         self.music_id = self.entry.options.get("music_id")
@@ -54,42 +54,44 @@ class ConversationAssistant(conversation.AbstractConversationAgent):
     @property
     def attribution(self):
         """Return the attribution."""
-        return {"name": "Powered by shaonianzhentan", "url": "https://github.com/shaonianzhentan/conversation"}
+        return {"name": "由 shaonianzhentan 提供技术支持", "url": "https://github.com/shaonianzhentan/conversation"}
 
     @property
-    def supported_languages(self) -> list[str] | Literal["*"]:
+    def supported_languages(self) -> list[str]:
         """Return a list of supported languages."""
-        return MATCH_ALL
+        return get_domains_and_languages()["homeassistant"]
 
     async def async_process(
         self, user_input: conversation.ConversationInput
     ) -> conversation.ConversationResult:
         """Process a sentence."""
-        raw_prompt = "系统提示"
 
-        if user_input.conversation_id in self.history:
-            conversation_id = user_input.conversation_id
-            messages = self.history[conversation_id]
-        else:
-            conversation_id = ulid.ulid()
-            prompt = self._async_generate_prompt(raw_prompt)
-            messages = [{"role": "system", "content": prompt}]
-
-        messages.append({"role": "user", "content": user_input.text})
+        # 兼容中文语句
+        language = user_input.language or self.hass.config.language
+        country = self.hass.config.country
+        if language == "zh-Hans":
+            language = "zh-cn"
+        elif language == "zh-Hant":
+            if country == "HK":
+                language = "zh-hk"
+            elif country == "TW":
+                language = "zh-tw"
 
         # 处理意图
         conversation_voice = self.hass.data[DATA_VOICE]
-        text = conversation_voice.trim_char(text)
+        text = conversation_voice.trim_char(user_input.text)
 
         # 调用内置服务
-        intent_response = await conversation.async_converse(
+        conversation_result = await conversation.async_converse(
                 hass=self.hass,
                 text=text,
                 conversation_id=conversation_id,
-                context=user_input.context
+                context=user_input.context,
+                language=language,
+                agent_id='homeassistant'
             )
-        response = intent_response.response
-        if response.error_code is not None:
+        intent_response = conversation_result.response
+        if intent_response.error_code is not None:
             # 插件意图
             intent_response = await conversation_voice.async_process(text)
 
@@ -98,20 +100,6 @@ class ConversationAssistant(conversation.AbstractConversationAgent):
             result = speech.get('speech')
             conversation_voice.update(text, result)
 
-        _LOGGER.debug("Response %s", result)
-
-        messages.append({"role": "system", "content": result})
-        self.history[conversation_id] = messages
-
         return conversation.ConversationResult(
             response=intent_response, conversation_id=conversation_id
-        )
-
-    def _async_generate_prompt(self, raw_prompt: str) -> str:
-        """Generate a prompt for the user."""
-        return template.Template(raw_prompt, self.hass).async_render(
-            {
-                "ha_name": self.hass.config.location_name,
-            },
-            parse_result=False,
         )
