2f40fd42369f6cda2c5a987174908b196a80b262
shaonianzhentan <ilovevar@gmail.com>
2023-08-07 17:13:12 +0800

    支持语音识别

--- custom_components/conversation_assistant/stt.py
+++ custom_components/conversation_assistant/stt.py
@@ -22,7 +22,7 @@ async def async_setup_entry(
 class ConversationSttEntity(stt.SpeechToTextEntity):
 
     def __init__(self, config_entry: ConfigEntry):
-        self.speech_key = config_entry.options.get('speech_key')
+        self.speech_key = config_entry.options.get('speech_key', '')
         self._attr_name = '语音助手STT'
         self._attr_unique_id = f"{config_entry.entry_id}-stt"
 
@@ -56,7 +56,11 @@ class ConversationSttEntity(stt.SpeechToTextEntity):
         return [stt.AudioChannels.CHANNEL_MONO]
 
     async def async_process_audio_stream(self, metadata: stt.SpeechMetadata, stt_stream) -> stt.SpeechResult:
+        if self.speech_key == '':
+            return stt.SpeechResult('未配置Azure语音服务密钥', stt.SpeechResultState.SUCCESS)
+
         text = None
+        is_recognized = False
         try:
             speech_key, service_region = self.speech_key, "eastasia"
             speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
@@ -66,9 +70,18 @@ class ConversationSttEntity(stt.SpeechToTextEntity):
 
             speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language="zh-CN", audio_config=audio_config)
             
+            def speech_recognized(evt):
+                print('RECOGNIZED: {}'.format(evt))
+                nonlocal is_recognized
+                is_recognized = True
+
+                nonlocal text
+                text = evt.result
+                print(text)
+
             # Connect callbacks to the events fired by the speech recognizer
             speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
-            speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+            speech_recognizer.recognized.connect(speech_recognized)
             speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
             speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
             speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
@@ -78,15 +91,16 @@ class ConversationSttEntity(stt.SpeechToTextEntity):
 
             async for audio_bytes in stt_stream:
                 stream.write(audio_bytes)
+                if is_recognized:
+                    print('识别结束')
+                    stream.close()
+                    speech_recognizer.stop_continuous_recognition()
+                    break
 
-            print('结束啦')
-            stream.close()
-            speech_recognizer.stop_continuous_recognition()
-            
         except Exception as err:
             print(err)
             _LOGGER.exception("Error processing audio stream: %s", err)
-            return stt.SpeechResult(None, stt.SpeechResultState.ERROR)        
+            return stt.SpeechResult(None, stt.SpeechResultState.ERROR)
 
         if text is None:
             print('没听到声音')
